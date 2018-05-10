94febb30e5cfd5598a1091721f56cbe25f3506b5
Ismael Santiago <ismaelse95@gmail.com>
2018-05-10 09:15:38 +0200

    Ficheor editado

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template, url_for
 import os
 
 app = Flask(__name__)
-#port = os.environ['PORT']
+port = os.environ['PORT']
 
 @app.route('/')
 def inicio():
 	return render_template('index.html')
 
-app.run('0.0.0.0', debug=True)
+app.run('0.0.0.0',int(port), debug=True)
