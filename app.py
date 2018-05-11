37d0820952cdbb4fcc6748f860670ced2c82308e
Ismael Santiago <ismaelse95@gmail.com>
2018-05-11 13:16:21 +0200

    Editado

--- app.py
+++ app.py
@@ -16,4 +16,12 @@ def contacta():
 def busperfil():
 	return render_template('busperfil.html')
 
+@app.route('/formulario')
+def formulario():
+	return render_template('formulario.html')
+
+@app.route('/pagina1')
+def pagina1():
+	return render_template('pagina1.html')
+
 app.run('0.0.0.0',int(port), debug=True)
