20b3d4c2c9d6a4765975c179234b9ad3f8e11c18
Ismael Santiago <ismaelse95@gmail.com>
2018-05-17 12:22:35 +0200

    Fichero editados

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request,session
 import requests
 import os
 
 app = Flask(__name__)
+#app.secret_key="sdhaksjhdkasjdhsakjddksa" --> clave para una sesion nueva.
 port = os.environ['PORT']
 
 @app.route('/')
@@ -24,6 +25,7 @@ def busperfil(nombre=None, imagen=None, nombreusuario=None, steamid2=None, idiom
 		if r.status_code==200:
 			res=r.json()
 			ide=res["response"]["steamid"]
+			#session["id"]=ide --> para iniciar una sesión.
 			payload2={"key":"42F6A0CFD74A7E1A887BD94BEC654B62","steamids":ide}
 			r2=requests.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/",params=payload2)
 			if r.status_code==200:
@@ -93,5 +95,55 @@ def logros():
 							except KeyError:
 								errores+=1
 						return render_template('logrosresultado.html',juegos_lista2=juegos_lista2)
+
+@app.route('/juegosperfil/<idnuevo>',methods=["post","get"])
+def juegosperfil(idnuevo):
+	if request.method == "POST":
+		#id=session["id"] --> Para usar el session
+		return render_template('juegosperfil.html')
+	else:
+		ide=idnuevo
+		payload={"key":"42F6A0CFD74A7E1A887BD94BEC654B62","steamid":ide,"format":"json"}
+		r=requests.get("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001",params=payload)
+		if r.status_code==200:
+			resultado=r.json()
+			juegos=[]
+			for elem in resultado["response"]["games"]:
+				juegos.append(elem["appid"])
+				cuenta_juegos=len(juegos)
+			r2=requests.get("http://api.steampowered.com/ISteamApps/GetAppList/v2")
+			if r.status_code==200:
+				res3=r2.json()
+				juegos_lista=[]
+				for elem in res3["applist"]["apps"]:
+					for elem2 in juegos:
+						if elem["appid"] == elem2:
+							juegos_lista.append(elem["name"])
+		return render_template('juegosperfil.html', cuenta_juegos=cuenta_juegos, juegos_lista=juegos_lista)
+
+@app.route('/amigos/<idnuevo>',methods=["post","get"])
+def amigos(idnuevo):
+	if request.method == "POST":
+		return render_template('amigos.html')
+	else:
+		ide=idnuevo
+		payload={"key":"42F6A0CFD74A7E1A887BD94BEC654B62","steamid":ide,"relationship":"friend"}
+		r=requests.get("http://api.steampowered.com/ISteamUser/GetFriendList/v0001",params=payload)
+		if r.status_code==200:
+			resultado=r.json()
+			amigos=[]
+			lista_amigos=[]
+			for elem in resultado["friendslist"]["friends"]:
+				amigos.append(elem["steamid"])
+			for elem in amigos:
+				ide2=elem
+				payload2={"key":"42F6A0CFD74A7E1A887BD94BEC654B62","steamids":ide2}
+				r2=requests.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/",params=payload2)
+				if r.status_code==200:
+					resultado2=r2.json()
+					nombreusuario=resultado2["response"]["players"][0]["personaname"]
+					lista_amigos.append(nombreusuario)
+			return render_template('amigos.html',lista_amigos=lista_amigos)
+
 
 app.run('0.0.0.0',int(port), debug=True)
